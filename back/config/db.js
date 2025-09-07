fce1dbbab02bcfeb669e11becf369a6e51164a23
GebrekidanM <designerkidan@gmail.com>
2025-09-07 16:45:00 +0300

    mongo db

--- back/config/db.js
+++ back/config/db.js
@@ -2,14 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('✅ MongoDB Connected...');
   } catch (err) {
     console.error(err.message);
-    // Exit process with failure
     process.exit(1);
   }
 };
