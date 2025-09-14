b0286574c2727d96287673d8f5bd5ea9426f9ff3
GebrekidanM <designerkidan@gmail.com>
2025-09-14 06:34:09 +0300

    configure1

--- front/src/app/page.js
+++ front/src/app/page.js
@@ -2,14 +2,19 @@ import AboutMeSnippet from "../components/AboutMeSnippet";
 import HeroSection from "../components/HeroSection";
 import LatestBlogPosts from "../components/LatestBlogPosts";
 import NewsletterCTA from "../components/NewsletterCTA";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
 
 export default function Home() {
   return (
     <div>
+      <Navbar/>
       <HeroSection/>     
       <LatestBlogPosts/>  
       <AboutMeSnippet/> 
       <NewsletterCTA/>
+      <Footer/>
     </div>
   );
 }
